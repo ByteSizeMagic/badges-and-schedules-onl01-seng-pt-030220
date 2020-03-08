c045e339fbcdf9cf53b9f255a5845208e520f0fa
Crystal Schooling <bytesizemagic@gmail.com>
2020-03-08 11:35:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,8 +13,8 @@ end
 
 def assign_rooms(speaker_list)
   speaker_rooms = []
-  speaker_list.each_with_index do |speaker, room - 1|
-  welcome_message = "Hello, #{speaker}! You'll be assigned to #{room}!"
+  speaker_list.each_with_index do |speaker, room|
+  welcome_message = "Hello, #{speaker}! You'll be assigned to #{room -1}!"
   speaker_rooms.push(welcome_message)
   end
   return speaker_rooms
