ea3c18d83ff21220995aed8801fbd52207a6c675
Crystal Schooling <bytesizemagic@gmail.com>
2020-03-08 10:57:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speaker)
+  badge_messages = []
+  speaker.each do |speaker|
+    badge_message = badge_maker(speaker)
+    badge_messages.push(badge_message)
+  end
+  return badge_messages
+end
+
+def assign_rooms(speaker_list)
+
+end
+
+def printer
+  puts batch_badge_creator
+  puts assign_rooms
+end
